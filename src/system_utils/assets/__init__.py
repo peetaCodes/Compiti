0bc026d4d32404e45341333fda90b1c473a3da01
peetaCodes <pietro.mario.bellizio@gmail.com>
2026-04-15 15:54:15 +0200

    Added a file that was somehow excluded in the previous major commit.

---
+++ src/system_utils/assets/__init__.py
@@ -0,0 +1,11 @@
+import sys
+from pathlib import Path
+
+PROJ_DIR = Path(sys.path[1])
+ASSETS_DIR = PROJ_DIR / "assets"
+IMAGES_DIR = ASSETS_DIR / "images"
+FONTS_DIR = ASSETS_DIR / "fonts"
+
+FONTS = {file.name.partition(".")[0]: str(file.absolute()) for file in FONTS_DIR.glob("*.*")}
+
+#print(FONTS, FONTS_DIR, FONTS_DIR.glob("*.*"))
